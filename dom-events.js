5ad4db8131f475850deef35694b798bc2e072817
Sarah Bland <scb4ga@virginia.edu>
2018-01-03 13:00:03 -0500

    Add files via upload

--- dom-events.js
+++ dom-events.js
@@ -15,17 +15,58 @@ const astrosEl = document.getElementById("astros")
 //   astrosEl.innerHTML = `<ul>${peopleEls.join("")}</ul>`
 // }
 
- // create a function that alerts "these are all the astronauts!" when the title element is clicked.
+//N O T E: what object comes with the object listener?
 
- // log to the console the client's x coordinate of their mouse as they move it around the window.
+// create a function that alerts "these are all the astronauts!" when the title element is clicked.
+var title = document.getElementsByClassName('title')[0]
+console.log(title)
+title.onclick = () => {
+  alert("You clicked on a title! Congratulations!")
+}
+
+// log to the console the client's x coordinate of their mouse as they move it around the window.
+document.onmousemove = () => {
+  var x = event.clientX;     // get horizontal coordinate
+  var y = event.clientY;     // get vertical coordinate
+  var coor = "X coords: " + x + ", Y coords: " + y;
+  console.log(coor)
+}
 
 // make an element that displays the most recent key pressed in the DOM
+// make a elem, div, span, etc.
+var keyDiv = document.getElementById("key")
+console.log(keyDiv)
+document.onkeypress = () => {
+  let char = event.keyCode
+  //console.log("You pressed a key.") // tells us we pressed a key
+  keyDiv.innerHTML = String.fromCharCode(char)
+}
 
-// create a text input element. When a user clicks on the text field to input, log the input element to console.
+// create a text input element. When a user clicks on the text field to input, log the input
+// element to console.
+var input = document.getElementById("input0")
+console.log(input)
+input.onclick = () => {
+  console.log(input)
+}
 
-// when a user clicks away from the input, log ("bye") to the console.
+// when a user clicks away from the input, console.log ("bye") to the console.
 
-// wrap the input element with a form element.
+//GOAL render input text to the DOM s.t. don't have to refresh the whole page every time
+// wrap the input element with a form element. (i.e. <form></form> tag)
+const form = document.getElementById("almost lunch")
+//want to pass in an event that stores data specific to the event that occured ()
+//preventDefault prevents full refresh (which is the default)
+const textInput = document.getElementById("textInput")
+const answer = document.getElementById("answer")
+form.onsubmit = (event) => {
+    event.preventDefault()
+    //get text that was typed in AND render to the domString
+    console.log(textInput.value)
+    answer.innerText = textInput.value
+}
+//innerHTML // HTML that actually has li texts (HTML elements w/in)
+//innerText // string that you literally want to render as is
 
 // when the form is submitted, render the client's inputted text to the DOM
 
--- practice.js
+++ practice.js
@@ -3,27 +3,59 @@ console.log(peopleInSpace)
 
 const numberOfAstrosInSpace = (data) => {
   //return the number of astronauts in space right now, using the data
+  return data["number"]
 }
 console.log("number of people in space: ", numberOfAstrosInSpace(peopleInSpace))
 
-
 const astroNames = (data) => {
-  // return an array containing the name strings of the astronauts in space
-}
-console.log("names of people in space: ", astroNames(peopleInSpace))
+  // // return an array containing the name strings of the astronauts in space
+  // let names = []
+  // let astroData = data["people"]
+  // for (i = 0; i < astroData.length; i++) {
+  //   names += astroData[i]["name"] + " "
+  // }
+  // return names
 
+  let people = data.people
+  let nameArray = people.map((person) => { return person.name })
+  //console.log(nameArray)
+  return nameArray
+}
+
+console.log("names of people in space: ", astroNames(peopleInSpace))
 
 const allInSameCraft = (data) => {
   // return a boolean that specifies whether all astronauts are in the same space craft
+  // let crafts = []
+  // let craftData = data["people"]
+  // for (i = 0; i < craftData.length; i++) {
+  //   crafts += craftData[i]["craft"] + " "
+  //   for (j = 0; j < crafts.length - 1;  j++) {
+  //     let bool = (crafts[j] = crafts[j+1]) ? true : false
+  //     if(bool = "false") {
+  //       return bool
+  //     }
+  //   }
+  // }
+  // return bool
+
+  // let people = data.people
+  // let craftArray = people.map((person) => { return person.craft })
+  // console.log(craftArray)
+  //
+  // for(i = 0; i < craftArray.length-1; i++) {
+  //   if (craftArray[i] != craftArray[i+1]) {
+  //     return false
+  //   }
+  // }
+  // return true
 }
 console.log("same craft? ", allInSameCraft(peopleInSpace))
-
 
 const successfulResponse = (data) => {
   // return a boolean that specifies whether the response from the Open Notify API was successful
 }
 console.log("successful response? ", successfulResponse(peopleInSpace))
-
 
 const wheresJoe = (data) => {
   // return "in space!" if Joe Acaba is in space right now. Otherwise, return "dunno."
@@ -31,4 +63,31 @@ const wheresJoe = (data) => {
 console.log("where's Joe? ", wheresJoe(peopleInSpace))
 
 // BONUS
-// Using your astroNames function, dynamically render each of the astronauts' names to the DOM in an unordered list when the page loads.
+// Using your astroNames function, dynamically render each of the astronauts' names to the DOM in an unordered list when the page loads
+// const greeting = document.getElementById("greeting")
+// console.log(greeting)
+
+//greeting.innerText = "oh, hey there"
+
+//"<li>""name string here""</li>"
+
+//need to take names and display them on the web page by putting them between tags
+//make windows on load fcn provide names
+
+// for(int i = 0; i < astroNames.length-1; i++) {
+//   console.log(astroNames[i])
+// }
+//<"li">nameArray<"/li">
+
+
+const list = document.getElementById("astroList")
+window.onload = () => {
+
+  //NEED TO TAKE NAME STRINGS AND TURN THEM TO LI VIA loop
+  let nameArray = astroNames(peopleInSpace)
+  //console.log(nameArray)
+  let listItems = nameArray.map((name) => { return "<li>" + name + "</li>" })
+  console.log(listItems)
+  domString = listItems.join("")
+  list.innerHTML = domString
+}
